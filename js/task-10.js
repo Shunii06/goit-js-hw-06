f54b166644ea20cc2285acf5d9fc5ba74d349f10
Piotrek <138370309+Shunii06@users.noreply.github.com>
2023-11-06 22:37:25 +0100

    task 10 ?

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,25 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector('#controls input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+const createBoxes = amount => {
+  const boxes = Array.from({ length: amount }, (_, index) => {
+    const boxSize = 30 + index * 10;
+    const box = document.createElement('div');
+    box.style.width = box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    return box;
+  });
+  boxesContainer.append(...boxes);
+};
+const destroyBoxes = () => {
+  boxesContainer.innerHTML = '';
+};
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value);
+  createBoxes(amount);
+});
+destroyButton.addEventListener('click', destroyBoxes);
