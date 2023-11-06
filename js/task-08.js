b015c8009c2d986c966e8e8fa80ef91ae9068208
Piotrek <138370309+Shunii06@users.noreply.github.com>
2023-11-06 22:04:32 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const emailValue = this.elements.email.value.trim();
+  const passwordValue = this.elements.password.value.trim();
+  if (!emailValue || !passwordValue) {
+    alert('Wypenij wszystkie pola!');
+  } else {
+    const formData = {
+      email: emailValue,
+      password: passwordValue
+};
+    console.log(formData);
+    this.reset();
+}
+});
+
+
+
+
+
 
